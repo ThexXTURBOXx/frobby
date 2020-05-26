ed94a50361477692d842002ce53cefe0579aa7dc
Daniel R. Grayson <dan@math.uiuc.edu>
2020-05-26 09:55:46 -0500

    apply patches used by Debian

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@
 
 #include <ctime>
 #include <cstdlib>
+#include <unistd.h>
 
 /** This function runs the Frobby console interface. the ::main
     function calls this function after having set up DEBUG-specific
